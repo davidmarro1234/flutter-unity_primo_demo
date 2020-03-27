1e566909491678b6121dfe3722b04da89775484b
David Marro <davidmarro@MBP-di-David.homenet.telecomitalia.it>
2020-03-27 14:48:49 +0100

    primo commit

---
+++ unity/DemoApp/Temp/il2cppOutput/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,228 @@
+﻿#include "il2cpp-config.h"
+
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+IL2CPP_EXTERN_C_BEGIN
+IL2CPP_EXTERN_C_END
+
+
+
+
+// 0x00000001 <id>j__TPar <>f__AnonymousType0`4::get_id()
+// 0x00000002 <seq>j__TPar <>f__AnonymousType0`4::get_seq()
+// 0x00000003 <name>j__TPar <>f__AnonymousType0`4::get_name()
+// 0x00000004 <data>j__TPar <>f__AnonymousType0`4::get_data()
+// 0x00000005 System.Void <>f__AnonymousType0`4::.ctor(<id>j__TPar,<seq>j__TPar,<name>j__TPar,<data>j__TPar)
+// 0x00000006 System.Boolean <>f__AnonymousType0`4::Equals(System.Object)
+// 0x00000007 System.Int32 <>f__AnonymousType0`4::GetHashCode()
+// 0x00000008 System.String <>f__AnonymousType0`4::ToString()
+// 0x00000009 System.Void Rotate::Start()
+extern void Rotate_Start_m0C36379106B8F7DD309CAC5E6D720D811129FA07 ();
+// 0x0000000A System.Void Rotate::Update()
+extern void Rotate_Update_m7EECA0DFF07C6BCA773478F66A949EE038D28330 ();
+// 0x0000000B System.Void Rotate::SetRotationSpeed(System.String)
+extern void Rotate_SetRotationSpeed_mD1F6C05B18C3D792AB9AF055FDBCA8128873F461 ();
+// 0x0000000C System.Void Rotate::.ctor()
+extern void Rotate__ctor_mC70374AA84BC2CA04CA039D1B56F5AF1B95D42CF ();
+// 0x0000000D MessageHandler MessageHandler::Deserialize(System.String)
+extern void MessageHandler_Deserialize_mA9693BA8D2C9FD955FEA9DD8E48D55E6584BB8DE ();
+// 0x0000000E T MessageHandler::getData()
+// 0x0000000F System.Void MessageHandler::.ctor(System.Int32,System.String,System.String,Newtonsoft.Json.Linq.JToken)
+extern void MessageHandler__ctor_mFD041017CFD2E7BE9B9CDB629921C6D155BF2D11 ();
+// 0x00000010 System.Void MessageHandler::send(System.Object)
+extern void MessageHandler_send_m0BF4951456C3CA50EEF785A982A6F10D92BD0C5E ();
+// 0x00000011 System.Void UnityMessage::.ctor()
+extern void UnityMessage__ctor_m76E68BAF631BE07953E806CA26C480BF23B2ED45 ();
+// 0x00000012 System.Void NativeAPI::onUnityMessage(System.String)
+extern void NativeAPI_onUnityMessage_mD44345499E1125EA37D67E2892D48D89B448B012 ();
+// 0x00000013 System.Void NativeAPI::.ctor()
+extern void NativeAPI__ctor_m8A9AEF251DEAC1AD4CAF201F962E5DADBA8F47C5 ();
+// 0x00000014 System.Int32 UnityMessageManager::generateId()
+extern void UnityMessageManager_generateId_m39045BEBA86C4467AD5549DFA0DDCDF9A7A9CB89 ();
+// 0x00000015 UnityMessageManager UnityMessageManager::get_Instance()
+extern void UnityMessageManager_get_Instance_m3571F06844422C9D5B2ADE29D69AE7B9E63ACF16 ();
+// 0x00000016 System.Void UnityMessageManager::set_Instance(UnityMessageManager)
+extern void UnityMessageManager_set_Instance_m25A5E4940172A7950BA2EED339C161A0C74B05AE ();
+// 0x00000017 System.Void UnityMessageManager::add_OnMessage(UnityMessageManager_MessageDelegate)
+extern void UnityMessageManager_add_OnMessage_m7B623FDD1EB187BC9593C933D5E01766FC8DB344 ();
+// 0x00000018 System.Void UnityMessageManager::remove_OnMessage(UnityMessageManager_MessageDelegate)
+extern void UnityMessageManager_remove_OnMessage_mE747C20C6838E183F2C60960D2C697D7997588F4 ();
+// 0x00000019 System.Void UnityMessageManager::add_OnFlutterMessage(UnityMessageManager_MessageHandlerDelegate)
+extern void UnityMessageManager_add_OnFlutterMessage_m702ECE2711E59769765096D023C48B7C40F493B3 ();
+// 0x0000001A System.Void UnityMessageManager::remove_OnFlutterMessage(UnityMessageManager_MessageHandlerDelegate)
+extern void UnityMessageManager_remove_OnFlutterMessage_m6EE57FCB3B88653B284550F2168F322B043F3366 ();
+// 0x0000001B System.Void UnityMessageManager::.cctor()
+extern void UnityMessageManager__cctor_mDD0387729BD393CF289CB737BC70BF82CFAC885B ();
+// 0x0000001C System.Void UnityMessageManager::Awake()
+extern void UnityMessageManager_Awake_mCBF7274D38E90447257210355EEBED184FF0C035 ();
+// 0x0000001D System.Void UnityMessageManager::SendMessageToFlutter(System.String)
+extern void UnityMessageManager_SendMessageToFlutter_m0ADC2E517A85054AE5BA1D3328255060B20A1570 ();
+// 0x0000001E System.Void UnityMessageManager::SendMessageToFlutter(UnityMessage)
+extern void UnityMessageManager_SendMessageToFlutter_m390277F4683DC0C0F3437F5C69A4BD8FAD6DA70E ();
+// 0x0000001F System.Void UnityMessageManager::onMessage(System.String)
+extern void UnityMessageManager_onMessage_mD1704644A461EFD311809BB58F82D36898713485 ();
+// 0x00000020 System.Void UnityMessageManager::onFlutterMessage(System.String)
+extern void UnityMessageManager_onFlutterMessage_m5BEF69E2E3423B8F8ACCB39BBF8286128B4C2B5A ();
+// 0x00000021 System.Void UnityMessageManager::.ctor()
+extern void UnityMessageManager__ctor_mFB3651A2237E489ED8746D5B8FC5B5AB95FE7CE7 ();
+// 0x00000022 System.Void UnityMessageManager_MessageDelegate::.ctor(System.Object,System.IntPtr)
+extern void MessageDelegate__ctor_m3EA48B06536871149F1A43C905528166D49F48C8 ();
+// 0x00000023 System.Void UnityMessageManager_MessageDelegate::Invoke(System.String)
+extern void MessageDelegate_Invoke_m2D199E1633D3EFBEA727164B8A3F7888E6A4F801 ();
+// 0x00000024 System.IAsyncResult UnityMessageManager_MessageDelegate::BeginInvoke(System.String,System.AsyncCallback,System.Object)
+extern void MessageDelegate_BeginInvoke_m89C256246D24A40498952C82CDD615B3507E4BA4 ();
+// 0x00000025 System.Void UnityMessageManager_MessageDelegate::EndInvoke(System.IAsyncResult)
+extern void MessageDelegate_EndInvoke_m778326112BD3D26CB9DE0FFC335E9A1A9353BC35 ();
+// 0x00000026 System.Void UnityMessageManager_MessageHandlerDelegate::.ctor(System.Object,System.IntPtr)
+extern void MessageHandlerDelegate__ctor_m58386047CB442F144728F0A568B26FDDDB0EE6EA ();
+// 0x00000027 System.Void UnityMessageManager_MessageHandlerDelegate::Invoke(MessageHandler)
+extern void MessageHandlerDelegate_Invoke_mF982647CDFA5782B9ABAF328FF4070492A364B43 ();
+// 0x00000028 System.IAsyncResult UnityMessageManager_MessageHandlerDelegate::BeginInvoke(MessageHandler,System.AsyncCallback,System.Object)
+extern void MessageHandlerDelegate_BeginInvoke_mFDBB4C951CAC28A4F0E08666050BB3F266FA927E ();
+// 0x00000029 System.Void UnityMessageManager_MessageHandlerDelegate::EndInvoke(System.IAsyncResult)
+extern void MessageHandlerDelegate_EndInvoke_m9BC3850452E21DEBBF15C7598D811D4D744633A6 ();
+static Il2CppMethodPointer s_methodPointers[41] = 
+{
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	Rotate_Start_m0C36379106B8F7DD309CAC5E6D720D811129FA07,
+	Rotate_Update_m7EECA0DFF07C6BCA773478F66A949EE038D28330,
+	Rotate_SetRotationSpeed_mD1F6C05B18C3D792AB9AF055FDBCA8128873F461,
+	Rotate__ctor_mC70374AA84BC2CA04CA039D1B56F5AF1B95D42CF,
+	MessageHandler_Deserialize_mA9693BA8D2C9FD955FEA9DD8E48D55E6584BB8DE,
+	NULL,
+	MessageHandler__ctor_mFD041017CFD2E7BE9B9CDB629921C6D155BF2D11,
+	MessageHandler_send_m0BF4951456C3CA50EEF785A982A6F10D92BD0C5E,
+	UnityMessage__ctor_m76E68BAF631BE07953E806CA26C480BF23B2ED45,
+	NativeAPI_onUnityMessage_mD44345499E1125EA37D67E2892D48D89B448B012,
+	NativeAPI__ctor_m8A9AEF251DEAC1AD4CAF201F962E5DADBA8F47C5,
+	UnityMessageManager_generateId_m39045BEBA86C4467AD5549DFA0DDCDF9A7A9CB89,
+	UnityMessageManager_get_Instance_m3571F06844422C9D5B2ADE29D69AE7B9E63ACF16,
+	UnityMessageManager_set_Instance_m25A5E4940172A7950BA2EED339C161A0C74B05AE,
+	UnityMessageManager_add_OnMessage_m7B623FDD1EB187BC9593C933D5E01766FC8DB344,
+	UnityMessageManager_remove_OnMessage_mE747C20C6838E183F2C60960D2C697D7997588F4,
+	UnityMessageManager_add_OnFlutterMessage_m702ECE2711E59769765096D023C48B7C40F493B3,
+	UnityMessageManager_remove_OnFlutterMessage_m6EE57FCB3B88653B284550F2168F322B043F3366,
+	UnityMessageManager__cctor_mDD0387729BD393CF289CB737BC70BF82CFAC885B,
+	UnityMessageManager_Awake_mCBF7274D38E90447257210355EEBED184FF0C035,
+	UnityMessageManager_SendMessageToFlutter_m0ADC2E517A85054AE5BA1D3328255060B20A1570,
+	UnityMessageManager_SendMessageToFlutter_m390277F4683DC0C0F3437F5C69A4BD8FAD6DA70E,
+	UnityMessageManager_onMessage_mD1704644A461EFD311809BB58F82D36898713485,
+	UnityMessageManager_onFlutterMessage_m5BEF69E2E3423B8F8ACCB39BBF8286128B4C2B5A,
+	UnityMessageManager__ctor_mFB3651A2237E489ED8746D5B8FC5B5AB95FE7CE7,
+	MessageDelegate__ctor_m3EA48B06536871149F1A43C905528166D49F48C8,
+	MessageDelegate_Invoke_m2D199E1633D3EFBEA727164B8A3F7888E6A4F801,
+	MessageDelegate_BeginInvoke_m89C256246D24A40498952C82CDD615B3507E4BA4,
+	MessageDelegate_EndInvoke_m778326112BD3D26CB9DE0FFC335E9A1A9353BC35,
+	MessageHandlerDelegate__ctor_m58386047CB442F144728F0A568B26FDDDB0EE6EA,
+	MessageHandlerDelegate_Invoke_mF982647CDFA5782B9ABAF328FF4070492A364B43,
+	MessageHandlerDelegate_BeginInvoke_mFDBB4C951CAC28A4F0E08666050BB3F266FA927E,
+	MessageHandlerDelegate_EndInvoke_m9BC3850452E21DEBBF15C7598D811D4D744633A6,
+};
+static const int32_t s_InvokerIndices[41] = 
+{
+	-1,
+	-1,
+	-1,
+	-1,
+	-1,
+	-1,
+	-1,
+	-1,
+	23,
+	23,
+	26,
+	23,
+	0,
+	-1,
+	40,
+	26,
+	23,
+	163,
+	23,
+	106,
+	4,
+	163,
+	26,
+	26,
+	26,
+	26,
+	3,
+	23,
+	26,
+	26,
+	26,
+	26,
+	23,
+	124,
+	26,
+	214,
+	26,
+	124,
+	26,
+	214,
+	26,
+};
+static const Il2CppTokenRangePair s_rgctxIndices[2] = 
+{
+	{ 0x02000002, { 0, 21 } },
+	{ 0x0600000E, { 21, 1 } },
+};
+static const Il2CppRGCTXDefinition s_rgctxValues[22] = 
+{
+	{ (Il2CppRGCTXDataType)2, 21568 },
+	{ (Il2CppRGCTXDataType)3, 15225 },
+	{ (Il2CppRGCTXDataType)2, 21569 },
+	{ (Il2CppRGCTXDataType)3, 15226 },
+	{ (Il2CppRGCTXDataType)3, 15227 },
+	{ (Il2CppRGCTXDataType)2, 21570 },
+	{ (Il2CppRGCTXDataType)3, 15228 },
+	{ (Il2CppRGCTXDataType)3, 15229 },
+	{ (Il2CppRGCTXDataType)2, 21571 },
+	{ (Il2CppRGCTXDataType)3, 15230 },
+	{ (Il2CppRGCTXDataType)3, 15231 },
+	{ (Il2CppRGCTXDataType)2, 21572 },
+	{ (Il2CppRGCTXDataType)3, 15232 },
+	{ (Il2CppRGCTXDataType)3, 15233 },
+	{ (Il2CppRGCTXDataType)3, 15234 },
+	{ (Il2CppRGCTXDataType)3, 15235 },
+	{ (Il2CppRGCTXDataType)3, 15236 },
+	{ (Il2CppRGCTXDataType)2, 21207 },
+	{ (Il2CppRGCTXDataType)2, 21208 },
+	{ (Il2CppRGCTXDataType)2, 21209 },
+	{ (Il2CppRGCTXDataType)2, 21210 },
+	{ (Il2CppRGCTXDataType)3, 15237 },
+};
+extern const Il2CppCodeGenModule g_AssemblyU2DCSharpCodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharpCodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	41,
+	s_methodPointers,
+	s_InvokerIndices,
+	0,
+	NULL,
+	2,
+	s_rgctxIndices,
+	22,
+	s_rgctxValues,
+	NULL,
+};
